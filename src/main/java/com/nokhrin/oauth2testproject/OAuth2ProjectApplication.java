73f29986eb603d8a26087392ce4c0c7e187936ad
yevheniinokhrin <233315@edu.p.lodz.pl>
2022-01-01 19:14:25 +0200

    first commit

---
+++ src/main/java/com/nokhrin/oauth2testproject/OAuth2ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.nokhrin.oauth2testproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth2ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OAuth2ProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nokhrin/oauth2testproject/OAuth2ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nokhrin.oauth2testproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth2ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
